df55a0d5626bc0728c2a2a4a72a41692c101aaa1
Subhaashree06 <subhaashree06@gmail.com>
2026-05-10 12:14:31 +0530

    Trigger GitHub Actions deployment

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Continuous Deployment using GitHub Actions"
+    return "Auto deployed using GitHub Actions CI/CD"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
